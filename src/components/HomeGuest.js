061787b85c9dfd9ed4af2dc2af75adace549029c
zulqarnain-99 <zulqi99@gmail.com>
2021-01-05 00:12:03 +0500

    remove console warnings

--- src/components/HomeGuest.js
+++ src/components/HomeGuest.js
@@ -11,19 +11,19 @@ function HomeGuest(){
       <div className="col-lg-5 pl-lg-5 pb-3 py-lg-5">
         <form>
           <div className="form-group">
-            <label for="username-register" className="text-muted mb-1">
+            <label htmlFor="username-register" className="text-muted mb-1">
               <small>Username</small>
             </label>
-            <input id="username-register" name="username" className="form-control" type="text" placeholder="Pick a username" autocomplete="off" />
+            <input id="username-register" name="username" className="form-control" type="text" placeholder="Pick a username" autoComplete="off" />
           </div>
           <div className="form-group">
-            <label for="email-register" className="text-muted mb-1">
+            <label htmlFor="email-register" className="text-muted mb-1">
               <small>Email</small>
             </label>
-            <input id="email-register" name="email" className="form-control" type="text" placeholder="you@example.com" autocomplete="off" />
+            <input id="email-register" name="email" className="form-control" type="text" placeholder="you@example.com" autoComplete="off" />
           </div>
           <div className="form-group">
-            <label for="password-register" className="text-muted mb-1">
+            <label htmlFor="password-register" className="text-muted mb-1">
               <small>Password</small>
             </label>
             <input id="password-register" name="password" className="form-control" type="password" placeholder="Create a password" />
