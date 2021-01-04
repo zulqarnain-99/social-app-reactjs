e1cbf022377b1cc05f5abbb31ec93a4a2ac503b4
zulqarnain-99 <zulqi99@gmail.com>
2021-01-05 00:07:18 +0500

    create footer, header and main components separately

---
+++ src/Main.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Header from './components/Header';
+import HomeGuest from './components/HomeGuest';
+import Footer from './components/Footer';
+
+function Main() {
+  return (
+    <>
+    <Header />
+    <HomeGuest />
+    <Footer/>
+    </>
+  );
+}
+
+export default Main;
+ 
